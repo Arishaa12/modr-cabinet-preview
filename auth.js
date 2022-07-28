09e6ce3b5a2c82fec821ab26487fe221ff00a85d
Nikita Shvetsov <hxshard@gmail.com>
2022-07-28 16:46:45 +0400

    upd

---
+++ auth.js
@@ -0,0 +1,3 @@
+if (location.search.includes("bc=1")) {
+    document.querySelector("#r").setAttribute("class", "i");
+}
--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-let alertTimeout;
-let alert = document.querySelector("#b");
+let refreshAlert = document.querySelector("#b");
 let beforeNameLabel = document.querySelector("#pn");
 let nameLabel = document.querySelector("#n");
 let subscriptionEndsLabel = document.querySelector("#se");
 let balanceLabel = document.querySelector("#bl");
+let mUIDInput = document.querySelector("#mui");
+let mUIDButton = document.querySelector("#mub");
+let mUIDAlert = document.querySelector("#mua");
+let transferInput = document.querySelector("#tri");
+let transferButton = document.querySelector("#trb");
+let transferAlert = document.querySelector("#tra");
 
 const AUTH_PAGE_URL = "https://test.modr.club/auth.html?bc=1";
 
 const STR = {
+    ERR: "Неизвестная ошибка.",
+    ERR_401: "Ошибка авторизации.",
+    ERR_500: "Внутренняя ошибка сервера.",
+    ERR_1000: "Произошла ошибка при подключении к серверу. Проверьте своё соединение с интернетом.",
+
     RL_USER_INF: "Обновляем отображаемую на странице информацию...",
-    RL_USER_INF_ERR: "На странице может отображаться <b>неверная</b> информация: ",
+    RL_USER_INF_ERR: "На странице может отображаться <b>устаревшая</b> информация: ",
     RL_USER_INF_CON_ERR: "произошла ошибка при подключении к серверу. Проверьте своё соединение с интернетом.",
     RL_USER_INF_SRV_ERR: "сервер вернул ошибку при обновлении данных. Пожалуйста, подождите несколько минут и обновите страницу.",
 
@@ -18,11 +28,22 @@ const STR = {
     SE_EXP: "Кажется, ваша подписка уже закончилась. Это произошло <strong>{0}</strong>.",
     SE_ACT: "Ура, ваша подписка ещё активна. Закончится она <strong>{0}</strong>.",
 
+    BAL: "На вашем балансе сейчас <strong>{0}</strong>.",
+
     RUB_0: "рубль",
     RUB_1: "рубля",
     RUB_2: "рублей",
 
-    BAL: "На вашем балансе сейчас <strong>{0}</strong>."
+    MUID_SVG: "Сохраняем...",
+    MUID_SVD: "Сохранено!",
+    MUID_ERR_1: "В UID должно быть 24 символа!",
+    MUID_ERR_2: "UID введён неверно. Убедитесь, что вы вводите UID из уже установленного мода, а не из оригинала игры.",
+
+    TRF_TNG: "Переносим...",
+    TRF_TVD: "Перенесли!",
+    TRF_ERR_1: "В UID должно быть 24 символа!",
+    TRF_ERR_2: "Вы не привязали UID в разделе \"Привязка аккаунта\".",
+    TRF_ERR_3: "Убедитесь в том, что вы правильно переписали UID."
 };
 
 function extractCookie(name) {
@@ -63,24 +84,36 @@ async function executeMethod(method, params = {}) {
     throw("NOT_OK");
 }
 
-function hideAlert() {
+function hideAlert(alert) {
+    if (alert.alertTimeout) {
+        clearTimeout(alert.alertTimeout);
+    }
+
     alert.setAttribute("class", "h");
 }
 
-function showAlert(message, duration, error = false) {
-    if (alertTimeout) {
-        clearTimeout(alertTimeout);
-    }
+function showAlert(alert, message, duration = 0, status = 0) {
+    hideAlert(alert);
 
     if (duration > 0) {
-        alertTimeout = setTimeout(
-            hideAlert, 
+        alert.alertTimeout = setTimeout(
+            () => hideAlert(alert), 
             duration * 1000
         );
     }
 
     alert.innerHTML = message;
-    alert.setAttribute("class", error ? "i e" : "i");
+    alert.setAttribute("class", status == 0 ? "i" : (
+        status == 1 ? "i g" : "i e"
+    ));
+}
+
+function getErrorByCode(code, section) {
+    if (STR["ERR_" + code]) return STR["ERR_" + code];
+    if (STR[section + "_ERR_" + code])
+        return STR[section + "_ERR_" + code];
+    
+    return STR.ERR;
 }
 
 String.prototype.format = function() {
@@ -125,22 +158,103 @@ function setSubscriptionEnds(subscriptionends) {
         .format(ftdDate);
 }
 
+function unlockButtons() {
+    document.querySelectorAll(".d").forEach(btn => {
+        btn.removeAttribute("disabled");
+    });
+}
+
 async function updateDisplayInfo() {
-    showAlert(STR.RL_USER_INF, 0);
+    showAlert(refreshAlert, STR.RL_USER_INF);
 
     try {
         const userData = await executeMethod("user");
         const { balance, subscriptionends, muid } = userData;
 
-        hideAlert();
         setBalance(balance);
         setSubscriptionEnds(subscriptionends);
+        hideAlert(refreshAlert);
+
+        mUIDInput.value = muid;
+        unlockButtons();
     } catch (e) {
         if (e === "NOT_OK") {
-            return showAlert(STR.RL_USER_INF_ERR + STR.RL_USER_INF_SRV_ERR, 0, true);
+            return showAlert(refreshAlert, STR.RL_USER_INF_ERR + STR.RL_USER_INF_SRV_ERR, 0, 2);
         }
 
-        showAlert(STR.RL_USER_INF_ERR + STR.RL_USER_INF_CON_ERR, 0, true);
+        showAlert(refreshAlert, STR.RL_USER_INF_ERR + STR.RL_USER_INF_CON_ERR, 0, 2);
+    }
+}
+
+mUIDButton.onclick = async () => {
+    showAlert(mUIDAlert, STR.MUID_SVG);
+
+    const muid = 
+        mUIDInput.value;
+
+    if (muid.length !== 24) {
+        return showAlert(mUIDAlert, STR.MUID_ERR_1, 5, 2);
+    }
+
+    const result = 
+        await updateMUID(muid);
+
+    if (result.success) {
+        return showAlert(mUIDAlert, STR.MUID_SVD, 5, 1);
+    }
+
+    showAlert(mUIDAlert, getErrorByCode(result.err, "MUID"), 10, 2);
+};
+
+transferButton.onclick = async () => {
+    showAlert(transferAlert, STR.TRF_TNG);
+
+    const uid = 
+        transferInput.value;
+
+    if (uid.length !== 24) {
+        return showAlert(transferAlert, STR.TRF_ERR_1, 5, 2);
+    }
+
+    const result = 
+        await transferSaves(uid);
+
+    if (result.success) {
+        return showAlert(transferAlert, STR.TRF_TVD, 5, 1);
+    }
+
+    showAlert(transferAlert, getErrorByCode(result.err, "TRF"), 10, 2);
+};
+
+async function updateMUID(muid) {
+    try {
+        const result = await executeMethod("muid", {
+            muid
+        });
+
+        return result;
+    } catch (e) {
+        if (e === "NOT_OK") {
+            return { err: -1 };
+        }
+
+        return { err: 1000 };
+    }
+}
+
+async function transferSaves(uid) {
+    try {
+        const result = await executeMethod("transfer", {
+            uid
+        });
+
+        return result;
+    } catch (e) {
+        if (e === "NOT_OK") {
+            return { err: -1 };
+        }
+
+        return { err: 1000 };
     }
 }
 

9ef5b593b4968bc5636be5f708b04fb5e503e9ff
Nikita Shvetsov <hxshard@gmail.com>
2022-07-28 18:13:22 +0400

    upd

--- auth.js
+++ auth.js
@@ -1,3 +1,26 @@
+const vk = document.querySelector(".vk");
+const tg = document.querySelector(".tg");
+
 if (location.search.includes("bc=1")) {
-    document.querySelector("#r").setAttribute("class", "i");
+    document.querySelector("#r").innerHTML = "Кажется, ваша сессия устарела. Пожалуйста, авторизуйтесь снова.";
 }
+
+vk.onclick = () => {
+    vk.setAttribute("disabled", true);
+
+    setTimeout(() => {
+        vk.removeAttribute("disabled");
+    }, 3000);
+
+    location.href = "https://2d.modr.club/auth/redirect";
+};
+
+tg.onclick = () => {
+    tg.setAttribute("disabled", true);
+
+    setTimeout(() => {
+        tg.removeAttribute("disabled");
+    }, 3000);
+
+    location.href = "https://t.me/modrAuthBot";
+};
